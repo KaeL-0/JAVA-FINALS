641a0449bdf8f94c8b1869aa81cba75497e4b382
Luis Anton Gonzales <153941400+GonzalesLuis@users.noreply.github.com>
2024-11-30 00:02:42 +0800

    Update Catalogue.java

--- Catalogue.java
+++ Catalogue.java
@@ -28,16 +28,20 @@ class Catalogue {
     //Checks if the product exists in the catalogue, then returns a boolean
     public boolean checkCatalogue(int checkProductID){
 
+
         boolean productExist = false;
 
-        for (Product product : productList) {
-            if (product.getProductID() == checkProductID){
-                productExist = true;
+
+            for (Product product : productList) {
+                if (product.getProductID() == checkProductID) {
+                    productExist = true;
+                }
+
             }
 
-        }
 
         return productExist;
+
     }
 
     //Checks if the product exists in the catalogue. If true, returns the product
@@ -56,111 +60,115 @@ class Catalogue {
 
     //Removes product from catalogue
     public void removeCatalogueItem (){
-        //Display catalogue
-        viewCatalogue();
 
-        boolean productExist = false;
-        int productToRemove;
+        if(!productList.isEmpty()) {
+            //Display catalogue
+            viewCatalogue();
 
-        //Prompt to enter product ID of product to remove
-        System.out.print("Enter product ID: ");
-        productToRemove = input.nextInt();
-        input.nextLine();
+            boolean productExist = false;
+            int productToRemove;
 
-        Iterator<Product> iter = productList.iterator();
+            //Prompt to enter product ID of product to remove
+            System.out.print("Enter product ID: ");
+            productToRemove = input.nextInt();
+            input.nextLine();
 
-        //Traverse product list
-        while(iter.hasNext()){
-            Product product = iter.next();
+            Iterator<Product> iter = productList.iterator();
 
-            //If product exist, remove product
-            if (product.getProductID() == productToRemove){
-                productExist = true;
+            //Traverse product list
+            while (iter.hasNext()) {
+                Product product = iter.next();
 
-                iter.remove();
-                System.out.println("\n[Product removed]");
+                //If product exist, remove product
+                if (product.getProductID() == productToRemove) {
+                    productExist = true;
 
+                    iter.remove();
+                    System.out.println("\n[Product removed]");
+
+                }
             }
-        }
 
-        //Else display order does not exist
-        if (!productExist){
-            System.out.println("\n[Product does not exist]\n");
+            //Else display order does not exist
+            if (!productExist) {
+                System.out.println("\n[Product does not exist]\n");
+            }
+        } else {
+            System.out.println("[Catalogue is empty]");
         }
 
     }
 
-    public void updateCatalogueItem(){
-        int productToModify;
-        boolean productExist = false;
+    public void updateCatalogueItem() {
+            int productToModify;
+            boolean productExist = false;
 
-        //Display catalogue
-        viewCatalogue();
-
-
-
-        while (!productExist) {
-            //Display prompt to enter product ID of product to modify
-            System.out.print("Enter product ID: ");
-            productToModify = input.nextInt();
-            input.nextLine();
-
-            //Traverse product list to check if product exist
-            for (Product product : productList) {
-                boolean confirm = false;
+            //Display catalogue
+            viewCatalogue();
 
 
-                //If product exist, enter new product details
-                if (productToModify == product.getProductID()) {
+            if (!productList.isEmpty()) {
+                while (!productExist) {
+                    //Display prompt to enter product ID of product to modify
+                    System.out.print("Enter product ID: ");
+                    productToModify = input.nextInt();
+                    input.nextLine();
+
+                    //Traverse product list to check if product exist
+                    for (Product product : productList) {
+                        boolean confirm = false;
 
 
-                    while (!confirm) {
-                        System.out.println("\nPRODUCT\n");
-                        product.displayProduct();
+                        //If product exist, enter new product details
+                        if (productToModify == product.getProductID()) {
 
 
-                        productExist = true;
+                            while (!confirm) {
+                                System.out.println("\nPRODUCT\n");
+                                product.displayProduct();
 
 
-                        System.out.print("Enter new name: ");
-                        product.setProductName(input.nextLine());
+                                productExist = true;
 
 
-                        System.out.print("Enter new price: ");
-                        product.setProductPrice(input.nextDouble());
-                        input.nextLine();
+                                System.out.print("Enter new name: ");
+                                product.setProductName(input.nextLine());
 
-                        System.out.print("Enter new stock: ");
-                        product.setProductStock(input.nextInt());
-                        input.nextLine();
 
-                        System.out.print("Enter new description: ");
-                        product.setProductDescription(input.nextLine());
+                                System.out.print("Enter new price: ");
+                                product.setProductPrice(input.nextDouble());
+                                input.nextLine();
 
-                        System.out.println("\nUPDATED PRODUCT");
-                        product.displayProduct();
+                                System.out.print("Enter new stock: ");
+                                product.setProductStock(input.nextInt());
+                                input.nextLine();
 
-                        System.out.print("\nConfirm [Y/N]: ");
-                        String confirmChoice = input.nextLine();
+                                System.out.print("Enter new description: ");
+                                product.setProductDescription(input.nextLine());
 
-                        if (confirmChoice.equalsIgnoreCase("Y")){
-                            confirm = true;
-                            System.out.println("\n[Product updated]");
+                                System.out.println("\nUPDATED PRODUCT");
+                                product.displayProduct();
+
+                                System.out.print("\nConfirm [Y/N]: ");
+                                String confirmChoice = input.nextLine();
+
+                                if (confirmChoice.equalsIgnoreCase("Y")) {
+                                    confirm = true;
+                                    System.out.println("\n[Product updated]");
+                                }
+
+                            }
+
+
+                        }
+
+                        if (!productExist) {
+                            System.out.println("[Product does not exist]\n");
+
                         }
 
                     }
-
-
-
                 }
-
-                if (!productExist) {
-                    System.out.println("[Product does not exist]\n");
-
-                }
-
             }
         }
-
     }
-}
